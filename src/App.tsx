16a59465292473f4d105078195ee6857a4ba319d
Denisson Carvalho <denii.carvalho94@gmail.com>
2024-07-01 16:24:32 -0300

    chore: Update package.json and optimize image imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import logo from './assets/logo.svg';
 import './App.css';
 import { Button } from '.';
 
@@ -11,7 +11,7 @@ function App() {
     <>
       <div className="flex justify-between items-center">
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={logo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
