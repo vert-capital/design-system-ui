3f421ca156e4b8dab9caa80bed9915f3fca87db7
Denisson Carvalho <denii.carvalho94@gmail.com>
2024-08-19 11:36:18 -0300

    feat: Possibilidade de voltar ordenação para estado default

--- src/components/app/data-table/header.tsx
+++ src/components/app/data-table/header.tsx
@@ -1,6 +1,6 @@
-import { cn } from '@/lib';
-import { Column } from '@tanstack/react-table';
-import { ChevronDown, ChevronUp } from 'lucide-react';
+import { cn } from "@/lib";
+import { Column } from "@tanstack/react-table";
+import { ChevronDown, ChevronUp } from "lucide-react";
 
 type Props = {
   column: Column<any, unknown>;
@@ -9,25 +9,33 @@ type Props = {
   className?: string;
 };
 export function DataTableHeader({ title, column, isSort, className }: Props) {
+  const toggleSorting = () => {
+    const currentSort = column.getIsSorted();
+
+    if (currentSort === "asc") {
+      column.toggleSorting(true); // Muda para 'desc'
+    } else if (currentSort === "desc") {
+      column.clearSorting(); // Muda para 'nenhum'
+    } else {
+      column.toggleSorting(false); // Muda para 'asc'
+    }
+  };
+
   return (
     <div
       className={cn(
-        'flex justify-start items-center space-x-1',
+        "flex justify-start items-center space-x-1",
         className,
-        isSort ? 'cursor-pointer' : '',
+        isSort ? "cursor-pointer" : ""
       )}
-      onClick={
-        isSort
-          ? () => column.toggleSorting(column.getIsSorted() === 'asc')
-          : undefined
-      }
+      onClick={isSort ? toggleSorting : undefined}
     >
       <div className="text-xs select-none truncate" title={title}>
         {title}
       </div>
       {isSort ? (
         column.getSortIndex() >= 0 ? (
-          column.getIsSorted() === 'asc' ? (
+          column.getIsSorted() === "asc" ? (
             <div className="flex flex-col justify-center items-center">
               <ChevronUp className="h-3 w-3 stroke-[3] stroke-brand -mb-1.5" />
               <ChevronDown className="h-3 w-3 stroke-muted-foreground" />
