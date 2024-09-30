7f472e6004ec197b7a0c4e4119fe37efdfde326f
Denisson Carvalho <denii.carvalho94@gmail.com>
2024-09-30 17:09:36 -0300

    nova versao com melhorias no checkbox

--- src/components/app/form/checkbox-basic.tsx
+++ src/components/app/form/checkbox-basic.tsx
@@ -1,50 +1,50 @@
-import { cn } from '@/lib/utils';
-import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
-import { Check } from 'lucide-react';
-import * as React from 'react';
+import { cn } from "@/lib/utils";
+import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
+import { Check } from "lucide-react";
+import * as React from "react";
 
 interface CheckboxProps
   extends React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root> {
-  label: string;
+  label?: string;
   className?: string;
+  classNameLabel?: string;
+  children?: React.ReactNode;
 }
 
 const CheckBoxBasic = React.forwardRef<
   React.ElementRef<typeof CheckboxPrimitive.Root>,
   CheckboxProps
->(({ label, className, ...props }, ref) => {
+>(({ label, className, classNameLabel, children, ...props }, ref) => {
   return (
-    <div className={'flex items-center'}>
+    <div className={"flex items-center"}>
       <CheckboxPrimitive.Root
         id={props.value?.toString()}
         ref={ref}
         className={cn(
-          `
-              peer h-5 w-5 shrink-0 rounded border border-muted-foreground ring-neutral_high-extra_light
-              focus-visible:outline-none focus-visible:ring-4
-              ${
-                !props.disabled
-                  ? 'data-[state=checked]:bg-brand data-[state=checked]:text-background bg-background'
-                  : 'data-[state=checked]:text-muted-foreground opacity-50 bg-neutral_high-medium text-muted-foreground cursor-not-allowed'
-              }
-           `,
+          `peer h-5 w-5 shrink-0 rounded border border-muted-foreground ring-neutral_high-extra_light focus-visible:outline-none focus-visible:ring-4 ${
+            !props.disabled
+              ? "bg-background data-[state=checked]:bg-brand data-[state=checked]:text-background"
+              : "cursor-not-allowed bg-neutral_high-medium text-muted-foreground opacity-50 data-[state=checked]:text-muted-foreground"
+          } `,
           className,
         )}
         {...props}
       >
         <CheckboxPrimitive.Indicator
-          className={cn('flex items-center justify-center text-current')}
+          className={cn("flex items-center justify-center text-current")}
         >
-          <Check className="w-3.5 h-3.5" strokeWidth={3} />
+          <Check className="h-3.5 w-3.5" strokeWidth={3} />
         </CheckboxPrimitive.Indicator>
       </CheckboxPrimitive.Root>
+
       <label
-        className={
-          'ml-2 text-[0.813rem] leading-normal text-neutral_low font-normal'
-        }
-        htmlFor={label}
+        className={cn(
+          "ml-2 text-[0.813rem] font-normal leading-normal text-neutral_low",
+          classNameLabel,
+        )}
+        htmlFor={props.value?.toString()}
       >
-        {label}
+        {children || label}
       </label>
     </div>
   );
