75a1a0717ce2d10b8f3ecebd16568c9384d7b3d6
Denisson Carvalho <denii.carvalho94@gmail.com>
2024-10-03 15:58:01 -0300

    atualização versão 1.0.24 para alterar endereço do app de Lastros

--- src/components/app/applications-options.tsx
+++ src/components/app/applications-options.tsx
@@ -1,4 +1,4 @@
-import { LayoutGrid } from 'lucide-react';
+import { LayoutGrid } from "lucide-react";
 import {
   Button,
   DropdownMenu,
@@ -8,7 +8,7 @@ import {
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
-} from '../ui';
+} from "../ui";
 
 type AppProps = {
   name: string;
@@ -26,49 +26,49 @@ type Props = {
 export function ApplicationsOptions({ apps }: Props) {
   const _apps: AppProps[] = apps || [
     {
-      src: 'https://vertc-sso-prd.s3.amazonaws.com/apps/vertical-vert-c-ops.svg',
-      name: 'VERT-C Operações',
-      link: 'https://app.operacoes.vert-capital.com/',
+      src: "https://vertc-sso-prd.s3.amazonaws.com/apps/vertical-vert-c-ops.svg",
+      name: "VERT-C Operações",
+      link: "https://app.operacoes.vert-capital.com/",
     },
     {
-      src: 'https://vertc-sso-prd.s3.amazonaws.com/apps/vertical-vert-c-obrigacoes.svg',
-      name: 'VERT-C Obrigações',
-      link: 'https://obrigacoes.vert-capital.com/',
+      src: "https://vertc-sso-prd.s3.amazonaws.com/apps/vertical-vert-c-obrigacoes.svg",
+      name: "VERT-C Obrigações",
+      link: "https://obrigacoes.vert-capital.com/",
     },
     {
-      src: 'https://vertc-sso-prd.s3.amazonaws.com/apps/vertical-vert-c-triagem-docs.svg',
-      name: 'VERT-C Triagem Docs',
-      link: 'https://triagem.vert-capital.app/',
+      src: "https://vertc-sso-prd.s3.amazonaws.com/apps/vertical-vert-c-triagem-docs.svg",
+      name: "VERT-C Triagem Docs",
+      link: "https://triagem.vert-capital.app/",
     },
     {
-      src: 'https://vertc-sso-prd.s3.amazonaws.com/apps/vertical-vert-c-lastros.svg',
-      name: 'VERT-C Lastros',
-      link: 'https://gestora.vert-capital.app/',
+      src: "https://vertc-sso-prd.s3.amazonaws.com/apps/vertical-vert-c-lastros.svg",
+      name: "VERT-C Lastros",
+      link: "https://lastros.operacoes.vert-capital.app/",
     },
     {
-      src: 'https://vertc-sso-prd.s3.amazonaws.com/apps/icon-vert-c-painel-gestao.svg',
-      name: 'VERT-C Painel de gestão',
-      link: 'https://gestao.vert-capital.app/',
+      src: "https://vertc-sso-prd.s3.amazonaws.com/apps/icon-vert-c-painel-gestao.svg",
+      name: "VERT-C Painel de gestão",
+      link: "https://gestao.vert-capital.app/",
     },
   ];
 
   const goLink = (link: string) => {
-    window.open(link, '_self');
+    window.open(link, "_self");
   };
 
   const goAll = () => {
-    window.open('https://id.vert-capital.com/', '_self');
+    window.open("https://id.vert-capital.com/", "_self");
   };
 
   return (
     <TooltipProvider delayDuration={0}>
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
-          <div className="relative rounded-full  flex justify-end items-center space-x-3 cursor-pointer text-muted-foreground">
+          <div className="relative flex cursor-pointer items-center justify-end space-x-3 rounded-full text-muted-foreground">
             <Tooltip delayDuration={400}>
               <TooltipTrigger asChild>
-                <Button variant={'ghost'} size={'icon'}>
-                  <LayoutGrid className="h-4 w-4 stroke-muted-foreground fill-muted-foreground" />
+                <Button variant={"ghost"} size={"icon"}>
+                  <LayoutGrid className="h-4 w-4 fill-muted-foreground stroke-muted-foreground" />
                 </Button>
               </TooltipTrigger>
               <TooltipContent side="bottom" className="flex items-center gap-4">
@@ -78,19 +78,19 @@ export function ApplicationsOptions({ apps }: Props) {
           </div>
         </DropdownMenuTrigger>
         <DropdownMenuContent className="w-[22rem]" align="end" forceMount>
-          <div className="flex flex-col p-4 pb-0 space-y-3 justify-center items-center w-full">
-            <div className="w-full h-full grid gap-4 grid-cols-3">
+          <div className="flex w-full flex-col items-center justify-center space-y-3 p-4 pb-0">
+            <div className="grid h-full w-full grid-cols-3 gap-4">
               {_apps.map((app) => (
                 <Tooltip delayDuration={400} key={app.name}>
                   <TooltipTrigger asChild>
                     <div
                       onClick={() => goLink(app.link)}
-                      className="flex flex-col items-center justify-center rounded-xl border-2 cursor-pointer hover:bg-brand/10"
+                      className="flex cursor-pointer flex-col items-center justify-center rounded-xl border-2 hover:bg-brand/10"
                     >
                       <img
                         src={app.src}
                         alt={app.name}
-                        className="w-24 h-[auto] rounded-sm"
+                        className="h-[auto] w-24 rounded-sm"
                       />
                     </div>
                   </TooltipTrigger>
@@ -103,7 +103,7 @@ export function ApplicationsOptions({ apps }: Props) {
                 </Tooltip>
               ))}
             </div>
-            <Button variant={'ghost'} onClick={goAll}>
+            <Button variant={"ghost"} onClick={goAll}>
               Ver todas as aplicações
             </Button>
           </div>
